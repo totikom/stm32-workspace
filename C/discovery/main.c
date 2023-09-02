a0f80501311f9c7701c18a9d4ccb75c121ce858a
Eugene Lomov <eugene.v.lomov@gmail.com>
2023-09-03 01:37:42 +0300

    Added more LEDs to discovery

--- C/discovery/main.c
+++ C/discovery/main.c
@@ -11,9 +11,12 @@
 #define GPIOE_MODER     (*(volatile unsigned long*)(GPIOE_BASE + 0x00))
 #define GPIOE_ODR       (*(volatile unsigned long*)(GPIOE_BASE + 0x14))
 
-#define LED_PIN         9
+#define LED_PIN_MIN     8
+#define LED_PIN_MAX     16
 
-void sleep(unsigned long sleep_time)
+#define DELAY           50000
+
+void sleep(volatile unsigned long sleep_time)
 {
     while(sleep_time--);
 }
@@ -23,15 +26,17 @@ int main()
     // Enable clock on GPIOE peripheral
     RCC_AHBENR = 1 << 21;
 
-    // Put GPIOE9 into output mode
-    GPIOE_MODER |= 0x01 << LED_PIN*2;
+    // Put GPIOE pins into output mode
+    for (int i = LED_PIN_MIN; i < LED_PIN_MAX; i++) {
+	    GPIOE_MODER |= 0x01 << i * 2;
+    }
 
     while(1)
     {
-        GPIOE_ODR = 1 << LED_PIN; // set LED pin high
-        sleep(100000);
-        GPIOE_ODR  = 0 << LED_PIN;  // set LED pin low
-        sleep(100000);
+	    for (int i = LED_PIN_MIN; i < LED_PIN_MAX; i++) {
+		    GPIOE_ODR = 1 << i; // set LED pin high
+		    sleep(DELAY);
+	    }
     }
 }
 
