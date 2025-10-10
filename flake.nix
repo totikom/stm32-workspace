42fedc6484eb7e8b1842221d17cbd893f74caff3
Eugene Lomov <eugene.lomov@protonmail.com>
2025-10-10 14:59:38 +0300

    Move to flake-based repo

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A flake that loads some packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      in
+      rec {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            stm32cubemx
+            gdb
+            openocd
+            stlink
+            cmake
+            gcc-arm-embedded
+          ];
+        };
+      }
+    );
+}
+
+
